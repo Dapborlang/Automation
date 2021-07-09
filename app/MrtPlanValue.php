2cb6968261ec94e7856cf2228cf22e57f77ef285
Dapborlang <dapbor88@gmail.com>
2021-07-09 09:44:27 +0530

    display both plan and rc

--- app/MrtPlanValue.php
+++ app/MrtPlanValue.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class MrtPlanValue extends Model
 {
     protected $guarded = ['id'];
+
+    function getPlanValueAttribute()
+    {
+      return $this->attributes['plan_value'] . ' (' . $this->attributes['rechage_value'].')';
+    }
+
+    public function plan_value_edit()
+    {
+      return $this->attributes['plan_value'];
+    }
 }
